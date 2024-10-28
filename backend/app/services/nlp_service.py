0654d21a87a25769a110be92623798398fe55809
SouravKumarBarman <souravkumarbarman1729@gmail.com>
2024-10-28 13:59:32 +0530

    gemini api working

--- backend/app/services/nlp_service.py
+++ backend/app/services/nlp_service.py
@@ -1,10 +1,11 @@
-from langchain_community.llms import Anthropic # Assuming you use OpenAI models; change as needed
 from app.services.pdf_service import extract_text_from_pdf
 from app.config import UPLOAD_FOLDER
 from dotenv import load_dotenv
 import os
+import google.generativeai as genai
 
 load_dotenv()  # Load variables from .env file
+secret_key=os.getenv("API_KEY")
 
 
 async def process_question(pdf_id: str, question: str):
@@ -12,11 +13,16 @@ async def process_question(pdf_id: str, question: str):
     filepath = f"{UPLOAD_FOLDER}/{pdf_id}"
     pdf_text = extract_text_from_pdf(filepath)
 
+    
+
+    genai.configure(api_key=secret_key)  # Set the API key for GenerativeAI
+    model = genai.GenerativeModel("gemini-1.5-flash")
+    
     # Use LangChain to get the answer
-    model = Anthropic(anthropic_api_key=os.getenv("ANTHROPIC_API_KEY"))
     prompt = f"Context: {pdf_text}\nQuestion: {question}\nAnswer:"
+    answer = model.generate_content(prompt)
 
     # Call the model with the prompt
-    answer = model(prompt=prompt)
     
-    return answer
+    
+    return answer.text
