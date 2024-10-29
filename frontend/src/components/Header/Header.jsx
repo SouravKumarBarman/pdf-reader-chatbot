798f34d62eb73321a0c4e19dd69caded3fd0a564
SouravKumarBarman <souravkumarbarman1729@gmail.com>
2024-10-29 10:33:16 +0530

    header completed

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState} from "react";
 import axios from "axios";
 
 const Header = () => {
@@ -32,16 +32,16 @@ const Header = () => {
   };
 
   // handler for open file uploader clicking on upload button
-  const handleFileUpload = () => document.getElementById("fileInput").click();
+  const handleFileUpload = () => document.getElementById("fileInput").click()
 
   return (
-    <div className="flex flex-row">
+    <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
       <div className="text-center ">
         <img className="p-4 " src="./logo.png" alt="App Logo" width="150" />
       </div>
 
-      <div className="ml-auto items-center">
-        <div className="flex justify-end">
+      <div className="flex items-center">
+        <div className="">
           {loading ? (
             <p>Loading.....</p>
           ) : error ? (
@@ -55,13 +55,19 @@ const Header = () => {
               </p>
             )
           )}
-
+        </div>
+        <div onClick={handleFileUpload}>
           {/* Upload button */}
-          <button className="upload-btn" onClick={handleFileUpload}>
-            Upload PDF
-          </button>
-          <button className="small" onClick={handleFileUpload}>
-            <img alt="Upload Button" />
+          <button  className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-black rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-black dark:text-black focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
+            <span className="flex items-center relative px-5 py-2.5 transition-all ease-in duration-75 bg-white rounded-md group-hover:bg-opacity-0">
+              <img
+                src="./upload_btn.png"
+                alt=""
+                width="25px"
+                className="mr-0 sm:mr-3"
+              />
+              <span className="hidden sm:inline">Upload PDF</span>
+            </span>
           </button>
         </div>
         <div>
